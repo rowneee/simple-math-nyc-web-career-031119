00ce1347cac290fbb782163ec1e110a5cb7c7119
Roni Shabo <roni.shabo@gmail.com>
2019-02-21 20:36:43 +0000

    Automatically backed up by Learn

---
+++ lib/math.rb
@@ -0,0 +1,44 @@
+def addition(num1, num2)
+  sum = num1 + num2
+  sum
+end
+
+puts addition(5, 4)
+
+def subtraction(num1, num2)
+  num = num1 - num2
+  num
+end
+
+puts subtraction(10, 5)
+
+def division(num1, num2)
+  quotient = num1 / num2
+  quotient
+end
+
+puts division(50, 2)
+
+def multiplication(num1, num2)
+  mult = num1 * num2 
+end
+
+puts multiplication(4, 30)
+
+def modulo(num1, num2)
+  num1 % num2 = 
+end
+
+puts modulo(34, 5)
+
+def square_root(num)
+  Math.sqrt(num)
+end
+
+puts square_root(81)
+
+def order_of_operation(num1, num2, num3, num4)
+  #Hint:  __ + (( __ * __ ) / __ )
+end
+
+puts order_of_operation(7,)
